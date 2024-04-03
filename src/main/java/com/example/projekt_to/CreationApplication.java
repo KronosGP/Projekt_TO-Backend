d7eae84aba913c4a99aeb23b743810e079be57d7
Kamil0k <kamil.porada@onet.pl>
2024-04-03 13:16:41 +0200

    create sidebar

--- src/main/java/com/example/projekt_to/CreationApplication.java
+++ src/main/java/com/example/projekt_to/CreationApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CreationApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(CreationApplication.class.getResource("creation-window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        stage.setTitle("Okno tworzenia");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/projekt_to/CreationController.java
@@ -0,0 +1,8 @@
+package com.example.projekt_to;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CreationController {
+
+}
